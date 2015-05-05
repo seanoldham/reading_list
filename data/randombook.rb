744697860db847f83b3db46a143c7b666dd0cbdd
Sean Oldham <seanosaur@me.com>
2015-05-05 17:15:31 -0500

    add current and next books to index, update randomizer

--- data/randombook.rb
+++ data/randombook.rb
@@ -1,1 +1,12 @@
-require 'yaml' and YAML.load_file('to-read.yaml').sample.each_pair { |author,title| puts "#{author} - #{title}" }
+require 'yaml'
+books = YAML.load_file('to-read.yaml')
+book = books.sample
+
+open('up-next.yaml', 'w') { |f|
+  f << "- author: #{book['author']}\n"
+  if /:/.match(book['title']) or /'/.match(book['title'])
+    f << "  title:  '#{book['title'].gsub(/'/, '&#39;')}'\n"
+  else
+    f << "  title:  #{book['title']}\n"
+  end
+}
