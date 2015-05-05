45351d2af4f4d4a6b9b0912c36ce1e28d8043b04
seanosaur <seanosaur@me.com>
2015-05-05 16:11:18 +0000

    refactor!

--- data/randombook.rb
+++ data/randombook.rb
@@ -1,20 +1,3 @@
-# load data file
 require 'yaml'
-books = YAML.load_file('to-read.yaml')
-
-#create empty array for book storage
-book_list = []
-
-# iterate through data file
-books.each do |book|
-  
-  # find book's author and title
-  author = book['author']
-  title = book['title']
-  
-  # add book's author and title to array
-  book_list.push("#{author} - #{title}")
-end
-
-# print out random book from list
-puts book_list.sample
+book = YAML.load_file('to-read.yaml').sample
+puts "#{book['author']} - #{book['title']}"
