13bb961cafeb0d1ba99887e24bbf01853610448d
Sean Oldham <seanosaur@me.com>
2015-12-28 16:29:08 -0500

    It's a bit hacky, but this fixes #4

--- processing/goodreads.rb
+++ processing/goodreads.rb
@@ -3,9 +3,19 @@ load "secrets.rb" # Needs KEY, SECRET, USER_ID constants
 
 client = Goodreads::Client.new(api_key: KEY, api_secret: SECRET)
 
-shelf = client.shelf(USER_ID, "to-read", sort: "author", per_page: 200)
-
 File.open("../data/to-read.yaml", "w") do |file|
+  
+  shelf = client.shelf(USER_ID, "to-read", sort: "author", per_page: 200, page: 1)
+  
+  shelf.books.each do |book|
+    author = book["book"]["authors"]["author"]["name"]
+    title = book["book"]["title"]
+    url = book["book"]["link"]
+    file.puts "- author: #{author}\n  title:  \"#{title}\"\n  url:    \"#{url}\""
+  end
+  
+  shelf = client.shelf(USER_ID, "to-read", sort: "author", per_page: 200, page: 2)
+  
   shelf.books.each do |book|
     author = book["book"]["authors"]["author"]["name"]
     title = book["book"]["title"]
