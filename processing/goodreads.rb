240fe2d9da5cacc1265af6e64b29f4d064cb9fe4
Sean Oldham <seanosaur@me.com>
2016-11-01 10:33:04 -0500

    use @bamos’s consistent ordering for Goodreads
    https://github.com/bamos/reading-list/commit/d44e42a600cb397fc17b51e7963d4503ef8b67ca

--- processing/goodreads.rb
+++ processing/goodreads.rb
@@ -4,23 +4,29 @@ load "secrets.rb" # Needs KEY, SECRET, USER_ID constants
 client = Goodreads::Client.new(api_key: KEY, api_secret: SECRET)
 
 File.open("../data/to-read.yaml", "w") do |file|
-  
+
   shelf = client.shelf(USER_ID, "to-read", sort: "author", per_page: 200, page: 1)
-  
-  shelf.books.each do |book|
+  sorted_books = shelf.books.sort_by do |book|
+    [book['book']['authors']['author']['name'], book['book']['title']]
+  end
+
+  sorted_books.each do |book|
     author = book["book"]["authors"]["author"]["name"]
     title = book["book"]["title"]
     url = book["book"]["link"]
     file.puts "- author: #{author}\n  title:  \"#{title}\"\n  url:    \"#{url}\""
   end
-  
+
   shelf = client.shelf(USER_ID, "to-read", sort: "author", per_page: 200, page: 2)
+  sorted_books = shelf.books.sort_by do |book|
+    [book['book']['authors']['author']['name'], book['book']['title']]
+  end
   
-  shelf.books.each do |book|
+  sorted_books.each do |book|
     author = book["book"]["authors"]["author"]["name"]
     title = book["book"]["title"]
     url = book["book"]["link"]
     file.puts "- author: #{author}\n  title:  \"#{title}\"\n  url:    \"#{url}\""
   end
-  
+
 end
