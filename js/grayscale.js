b125b8a9ecdc5652c17f64bf75f070a14b4b2e40
Sean Oldham <seanosaur@me.com>
2017-03-14 08:32:35 -0500

    styling stuff

--- js/grayscale.js
+++ js/grayscale.js
@@ -8,8 +8,10 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-brand").removeClass("hidden");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-brand").addClass("hidden");
     }
 });
 
